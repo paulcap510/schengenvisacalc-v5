eb4916a4a438648115c793d95e2ec303854997bc
Paul <paulcapob@gmail.com>
2024-02-12 17:10:52 +0900

    Made some stylistic changes to pages and added the proper Title

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,24 +2,26 @@ import './Login.css';
 
 const Login = () => {
   return (
-    <div className="login-container">
-      <div className="overlay">Feature coming soon</div>
-      <div className="login-heading">Login</div>
-      <form className="login-form">
-        <input
-          type="text"
-          placeholder="Username"
-          id="username"
-          name="username"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          id="password"
-          name="password"
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="login-wrapper">
+      <div className="login-container">
+        <div className="overlay">Feature coming soon</div>
+        <div className="login-heading">Login</div>
+        <form className="login-form">
+          <input
+            type="text"
+            placeholder="Username"
+            id="username"
+            name="username"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
